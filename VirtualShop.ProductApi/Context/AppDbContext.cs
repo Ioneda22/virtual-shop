80707f52fbf08740838605af730119c9404a64c8
Bruno Ioneda <brunohideoioneda@gmail.com>
2025-02-26 10:22:32 -0300

    Creating models 'Category' and 'Project' and the Database context

---
+++ VirtualShop.ProductApi/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VirtualShop.ProductApi.Models;
+
+namespace VirtualShop.ProductApi.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Product { get; set; }
+}
